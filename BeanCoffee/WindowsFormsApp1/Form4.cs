eb024ec3d9d9586a50114001b1863f5c4eaa0cc3
영진 최 <chyj0318@naver.com>
2019-03-11 10:12:14 +0900

    1

--- BeanCoffee/WindowsFormsApp1/Form4.cs
+++ BeanCoffee/WindowsFormsApp1/Form4.cs
@@ -27,13 +27,36 @@ namespace WindowsFormsApp1
 
             //판넬
             Panel panel1;
+            Panel panel2;
+
             panel1 = new Panel();
             panel1.BackColor = Color.FromArgb(237, 227, 183);
-            panel1.Size = new Size(1170, 700);
+            panel1.Size = new Size(580, 700);
             panel1.Location = new Point(50, 50);
             panel1.Region = Region.FromHrgn(COMMON_Create_Ctl.CreateRoundRectRgn(2, 2, panel1.Width, panel1.Height, 15, 15));
 
+            panel2 = new Panel();
+            panel2.BackColor = Color.FromArgb(237, 227, 183);
+            panel2.Size = new Size(580, 700);
+            panel2.Location = new Point(660, 50);
+            panel2.Region = Region.FromHrgn(COMMON_Create_Ctl.CreateRoundRectRgn(2, 2, panel1.Width, panel1.Height, 15, 15));
+
             this.Controls.Add(panel1);
+            this.Controls.Add(panel2);
+            ListView listView1 = new ListView();
+            listView1.Size = new Size(530, 500);
+            listView1.Location = new Point(25, 100);
+            listView1.Region = Region.FromHrgn(COMMON_Create_Ctl.CreateRoundRectRgn(2, 2, listView1.Width, listView1.Height, 15, 15));
+            listView1.BackColor = Color.FromArgb(224, 224, 224);
+
+            ListView listView2 = new ListView();
+            listView2.Size = new Size(530, 500);
+            listView2.Location = new Point(25, 100);
+            listView2.Region = Region.FromHrgn(COMMON_Create_Ctl.CreateRoundRectRgn(2, 2, listView2.Width, listView2.Height, 15, 15));
+            listView2.BackColor = Color.FromArgb(224, 224, 224);
+
+            panel1.Controls.Add(listView1);
+            panel2.Controls.Add(listView2);
         }
     }
 }
